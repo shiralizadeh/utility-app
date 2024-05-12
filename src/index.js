f4eec6793ea28920742a12afb6e556e919b795e1
Mohamad Shiralizadeh <mohamad.shir@gmail.com>
2024-05-12 13:41:19 +0200

    feat: log products

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import { random } from "./random/random.js";
 import { randomHash } from "./randomHash/randomHash.js";
+import { filterProducts } from "./shop/product.js";
 
+// random number
 console.log(random(1, 100));
+
+// random hash
 console.log(randomHash());
+
+// shop
+const samsungPhones = filterProducts('Samsung');
+
+console.log(samsungPhones);
--- src/shop/product.js
+++ src/shop/product.js
@@ -5,15 +5,14 @@ const products = [
   { id: 4, name: "OnePlus 9 Pro", price: 400 },
   { id: 5, name: "Xiaomi Mi 11", price: 500 },
   { id: 6, name: "Sony Xperia 1 III", price: 600 },
-  { id: 7, name: "LG Velvet 2", price: 700 },
+  { id: 7, name: "Samsung Galaxy S23", price: 700 },
   { id: 8, name: "Motorola Edge+", price: 800 },
   { id: 9, name: "Huawei P50", price: 900 },
   { id: 10, name: "Nokia 9 PureView", price: 1000 },
 ];
 
-
-function filterProduct(id) {
-  return products.filter((product) => product.id === id);
+function filterProducts(query) {
+  return products.filter((product) => product.name.search(query) !== -1);
 }
 
 function addProduct(product) {
@@ -32,10 +31,4 @@ function averagePrice() {
   );
 }
 
-export {
-  products,
-  filterProduct,
-  addProduct,
-  removeProduct,
-  averagePrice,
-}
+export { products, filterProducts, addProduct, removeProduct, averagePrice };
